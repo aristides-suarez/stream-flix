4553816988712952f3a7cc22c63c8a4872443d91
Arístides Suárez <aristides.suarez@agilecontent.com>
2025-05-28 11:50:13 +0100

    Refactor RatingService to use Movie and User entities for rating management

--- src/main/java/org/learning/streamflix/config/SecurityConfig.java
+++ src/main/java/org/learning/streamflix/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.learning.streamflix.config;
 
+import org.learning.streamflix.filters.JwtAuthenticationFilter;
 import org.learning.streamflix.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
--- src/main/java/org/learning/streamflix/services/RatingService.java
+++ src/main/java/org/learning/streamflix/services/RatingService.java
@@ -1,7 +1,11 @@
 package org.learning.streamflix.services;
 
+import org.learning.streamflix.controllers.entities.Movie;
 import org.learning.streamflix.controllers.entities.Rating;
+import org.learning.streamflix.controllers.entities.User;
+import org.learning.streamflix.repositories.MovieRepository;
 import org.learning.streamflix.repositories.RatingRepository;
+import org.learning.streamflix.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +16,20 @@ public class RatingService {
 
   @Autowired
   private RatingRepository ratingRepository;
+  @Autowired
+  private MovieRepository movieRepository;
+
+  @Autowired
+  private UserRepository userRepository;
 
   // Add a rating to a movie
   public Rating addRating(Long movieId, Long userId, Rating rating) {
-    rating.setMovieId(movieId);
-    rating.setUserId(userId);
+    Movie movie = movieRepository.findById(movieId)
+        .orElseThrow(() -> new RuntimeException("Movie not found"));
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new RuntimeException("User not found"));
+    rating.setMovie(movie);
+    rating.setUser(user);
     return ratingRepository.save(rating);
   }
 
@@ -34,7 +47,7 @@ public class RatingService {
   public void deleteRating(Long ratingId, Long userId) {
     Rating rating = ratingRepository.findById(ratingId)
         .orElseThrow(() -> new RuntimeException("Rating not found"));
-    if (!rating.getUserId().equals(userId)) {
+    if (!rating.getUser().getId().equals(userId)) {
       throw new RuntimeException("Unauthorized to delete this rating");
     }
     ratingRepository.deleteById(ratingId);
